6cbe103561db286e82f9f65214d247af9f4897e6
Melissa M. Dailey <mel.dailey@gmail.com>
2015-09-15 16:17:37 -0700

    name formated, working on year

--- username.rb
+++ username.rb
@@ -2,13 +2,26 @@
 # Run `rspec /spec/username_spec.rb` to get started.
 
 def format_name(first, last)
-  nil
+	 if first === ""
+	 	return nil
+	 elsif last === ""
+	 	return nil
+	 else
+	 	fd = first.downcase.gsub(/[^a-z]/i, "")
+	 	ld = last.downcase.gsub(/[^a-z]/i, "")
+	 	fd[0] + ld
+	 end 
 end
 
-def format_year
-  nil
+def format_year(year)
+	# year.to_s
+	new_year = year[2..3]
 end
 
-def build_username
-  nil
-end
+
+# def build_username(**arg)
+# 	arg.first
+#   something.format_name
+
+# end
+1999
